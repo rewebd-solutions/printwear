1e083438a8a5fdcdc0beb73af76fb902e6490a4a
rewebd-solutions <127234723+rewebd-solutions@users.noreply.github.com>
2023-08-27 15:10:21 +0530

    Update connection.js

--- server/database/connection.js
+++ server/database/connection.js
@@ -5,8 +5,8 @@ const connectDB = async () => {
     mongoose.set("strictQuery", false);
 
     // return await mongoose.connect('mongodb+srv://sit21it063:ifP2Q7QWXU4j3vFw@testcluster.2llp5qx.mongodb.net/?retryWrites=true&w=majority');
-    return await mongoose.connect('mongodb://127.0.0.1:27017/printwear'); // test
-    //  mongoose.connect("mongodb+srv://barath:987654321@cluster0.ooxe0gr.mongodb.net/?retryWrites=true&w=majority");
+    //return await mongoose.connect('mongodb://127.0.0.1:27017/printwear'); // test
+    return await mongoose.connect("mongodb+srv://barath:987654321@cluster0.ooxe0gr.mongodb.net/?retryWrites=true&w=majority");
   }
   catch (e) {
     console.log(e);
