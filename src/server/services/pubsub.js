455fcd6a0e47996791ea82bc7e2271396d73eb2e
Sachin / <sreesachin11226@gmail.com>
2025-09-02 21:54:13 +0530

    Update cloudbuild.yaml and pubsub.js to use PUBSUB_DESIGN_TOPIC environment variable

--- src/server/services/pubsub.js
+++ src/server/services/pubsub.js
@@ -10,7 +10,7 @@ const pubSub = new PubSub({
   },
   projectId: process.env.GOOGLE_PROJECT_ID,
 });
-const topicName = "generate-design"
+const topicName = process.env.PUBSUB_DESIGN_TOPIC;
 
 async function setup() {
   // 1️⃣ Ensure Topic exists
